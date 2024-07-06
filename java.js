b035250f9bd4f29ec6958f6795c12b501c311bc1
Sri Krishna <104660752+Ramji1424@users.noreply.github.com>
2024-07-06 15:28:42 +0530

    java.js

--- java.js
+++ java.js
@@ -1,15 +1,16 @@
-"use strict";
-const body_obj = document.querySelector("body");
 
-const guess_imp = document.querySelector(".guess");
-const big_number = document.querySelector(".number");
+'use strict';
+const body_obj = document.querySelector('body');
 
-const score_label = document.querySelector(".score");
-const msg_label = document.querySelector(".message");
-const high_score_label = document.querySelector(".high score");
+const guess_inp = document.querySelector('.guess');
+const big_number = document.querySelector('.number');
 
-const again_btn = document.querySelector(".again");
-const check = document.querySelector(".check");
+const score_lable = document.querySelector('.score');
+const msg_label = document.querySelector('.message');
+const high_score_label = document.querySelector('.highscore');
+
+const again_btn = document.querySelector('.again');
+const check = document.querySelector('.check');
 
 let total_score = 20;
 let max_high_score = 0;
@@ -17,22 +18,23 @@ let hidden_num = Math.random() * 20 + 1;
 hidden_num = Math.trunc(hidden_num);
 console.log(hidden_num);
 
-console.log(guess_imp.value);
+console.log(guess_inp.value);
 
-check.addEventListener("click", function (event) {
-  let imp_val = Number(guess_imp.value);
-  if (imp_val === hidden_num) {
-    msg_label.textContent = "correct";
-    body_obj.style.backgroundcolor = "green";
+check.addEventListener('click', function (event) {
+  let inp_val = Number(guess_inp.value);
+  if (inp_val === hidden_num) {
+    msg_label.textContent = 'correct';
+    body_obj.style.backgroundColor = 'green';
     big_number.textContent = hidden_num;
 
-    if ((total_score = max_high_score)) {
+    if (total_score > max_high_score) {
       max_high_score = total_score;
       high_score_label.textContent = max_high_score;
     }
-  } else if ((imp_val = hidden_num)) {
-    msg_label.textContent + "too high";
+  } else if (inp_val > hidden_num) {
+    msg_label.textContent = 'too high';
     total_score--;
-    score_label.textContent = total_score;
+    score_lable.textContent = total_score;
   }
+  
 });
